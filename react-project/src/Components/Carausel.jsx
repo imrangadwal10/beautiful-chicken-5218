1e64a95a201553727bda40920e82aad93d73dfcb
imrangadwal10 <imrangadwal777@gmail.com>
2022-10-02 11:59:04 +0530

    added

--- react-project/src/Components/Carausel.jsx
+++ react-project/src/Components/Carausel.jsx
@@ -5,7 +5,7 @@ function Carousell(){
         return (
             <>
             <Carousel itemPadding={[10, 10]} enableAutoPlay={true} autoPlaySpeed={3000}  itemsToShow={2.5}>
-                  <Image _hover={{transform:"scale(1.5)"}} borderRadius={10} src="https://cdn01.pharmeasy.in/dam/banner/banner/37176137470-POCK27.jpg"></Image>
+                  <Image  borderRadius={10} src="https://cdn01.pharmeasy.in/dam/banner/banner/37176137470-POCK27.jpg"></Image>
                   <Image borderRadius={10} src="https://cdn01.pharmeasy.in/dam/banner/banner/51e34800f6d-HURRAY.jpg"></Image>
                   <Image  borderRadius={10} src="https://cdn01.pharmeasy.in/dam/banner/banner/1255dcab13f-LABTEST450.jpg"></Image>
                   <Image  borderRadius={10} src="https://cdn01.pharmeasy.in/dam/banner/banner/52a0de28cfc-HP.jpg"></Image>
